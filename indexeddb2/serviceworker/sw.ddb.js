5dbd8caa0f9ea17e8dfe5a755d2f09c342bad616
cieszynski <admin@cieszynski.de>
2025-05-14 00:18:02 +0200

    handle_database_requests rewritten

--- indexeddb2/serviceworker/sw.ddb.js
+++ indexeddb2/serviceworker/sw.ddb.js
@@ -59,6 +59,10 @@ const handle_store_requests = (dbname, storename, http) => new Promise(async (re
             }));
             break;
         }
+        case 'POST': {
+
+            break;
+        }
         default:
             resolve(new JSONStatusResponse({
                 status: 405,
@@ -68,16 +72,17 @@ const handle_store_requests = (dbname, storename, http) => new Promise(async (re
     }
 });
 
-const handle_database_requests = (dbname, http) => new Promise(async (resolve, reject) => {
+const handle_database_requests = (request, dbname) => new Promise(async (resolve, reject) => {
 
-    switch (http.request.method) {
-        case 'GET': {
+    switch (request.method + dbname.endsWith('/')) {
+        // GET /ddb/db
+        case 'GETfalse': {
             const list = await databases();
 
-            if (list[dbname]) {
+            if ((version = list[dbname])) {
+
                 indexedDB.open(dbname).onsuccess = (event) => {
                     resolve(new JSONResponse({
-                        stores: Array.from(event.target.result.objectStoreNames),
                         version: event.target.result.version,
                         name: event.target.result.name
                     }));
@@ -88,71 +93,83 @@ const handle_database_requests = (dbname, http) => new Promise(async (resolve, r
             }));
             break;
         }
-        case 'POST':
-            const body = await http.request.json();
+
+        // GET /ddb/db/
+        case 'GETtrue': {
             const list = await databases();
 
-            if ((version = list[dbname])) { // dbname exists
+            dbname = dbname.slice(0, -1);
 
-                if ((body?.version ?? version) <= version) { // version is up to date
-                    resolve(new JSONStatusResponse({
-                        status: 200,
-                    }));
-                    return;
+            if ((version = list[dbname])) {
+                indexedDB.open(dbname).onsuccess = (event) => {
+                    resolve(new JSONResponse(
+                        Array.from(event.target.result.objectStoreNames)
+                    ));
+                    event.target.result.close();
                 }
+            } else resolve(new JSONStatusResponse({
+                status: 404,
+            }));
+            break;
+        }
+
+        // POST /ddb/db
+        case 'POSTfalse': {
+            const body = await request.json();
+            const req = indexedDB.open(dbname, body?.version ?? 1);
+            const response = {
+                status: 200,
             }
 
-            // new db OR newer version available
-            indexedDB.open(dbname, body?.version ?? 1).onupgradeneeded = (event) => {
-                const db = event.target.result;
-
-                Object.entries(body?.stores ?? {}).forEach(([storename, definition]) => {
-                    if (Array.from(db.objectStoreNames).includes(storename)) {
-                        // TODO save data
-                        db.deleteObjectStore(storename);
-                    }
-
-                    [keypath, ...indexes] = definition.split(/\s*(?:,)\s*/);
-
-                    const store = db.createObjectStore(
-                        storename, {
-                        keyPath: keypath.replace(/[\+@]/, ''),
-                        autoIncrement: /^[\+@]/.test(keypath)
-                    });
-
-                    indexes.forEach(indexName => {
-
-                        store.createIndex(
-                            indexName.replace(/[\*!]/, ''),
-                            indexName
-                                .split(/\+/)
-                                // at this point every keypath is an array
-                                .map(elem => elem.replace(/[\*!]/, ''))
-                                .reduce((prev, cur, idx) => {
-                                    switch (idx) {
-                                        case 0:
-                                            // indexName is keyPath:
-                                            return cur;
-                                        case 1:
-                                            // indexName is compound key
-                                            return [prev, cur];
-                                        default:
-                                            return [...prev, cur];
-                                    }
-                                }),
-                            {
-                                multiEntry: /^\*/.test(indexName),
-                                unique: /^\!/.test(indexName)
-                            });
-                    })
-                });
-
+            req.onerror = (event) => {
                 resolve(new JSONStatusResponse({
-                    status: 201,
+                    status: 409,
+                    error: {
+                        message: event.target.error.message,
+                        name: event.target.error.name
+                    }
                 }));
             }
 
-        case 'DELETE':
+            req.onupgradeneeded = (event) => {
+                const db = event.target.result;
+
+                Array.from(body?.stores ?? []).forEach(storeOptions => {
+                    if (Array.from(db.objectStoreNames).includes(storeOptions.name)) {
+                        // TODO save data
+                        db.deleteObjectStore(storeOptions.name);
+                    }
+
+                    const store = db.createObjectStore(
+                        storeOptions.name, {
+                        keyPath: storeOptions?.keyPath,
+                        autoIncrement: storeOptions?.autoIncrement
+                    });
+
+                    Array.from(storeOptions?.indexes ?? []).forEach(indexOptions => {
+
+                        store.createIndex(
+                            indexOptions.name,
+                            indexOptions.keyPath, {
+                            unique: indexOptions?.unique,
+                            multiEntry: indexOptions?.multiEntry
+                        })
+                    });
+                });
+
+                response.status = 201;
+                response.location = `/ddb/${dbname}`;
+            }
+
+            req.onsuccess = (event) => {
+                event.target.result.close();
+                resolve(new JSONStatusResponse(response));
+            }
+            break;
+        }
+
+        // DELETE /ddb/db
+        case 'DELETEfalse':
             break;
 
         default:
@@ -162,25 +179,30 @@ const handle_database_requests = (dbname, http) => new Promise(async (resolve, r
     }
 });
 
-const handle_ddb_requests = async (http) => {
+const handle_ddb_requests = async (request, ddb) => {
 
-    switch (http.request.method) {
-        case 'GET':
-            return new JSONResponse(await databases());
+    switch (request.method + ddb.endsWith('/')) {
+
+        // GET /ddb/
+        case 'GETtrue':
+            return new JSONResponse(await indexedDB.databases());
+
         default:
             return new JSONStatusResponse({
                 status: 405,
-            })
+            });
     }
 }
 
 self.addEventListener("fetch", async (event) => {
     console.debug('fetch');
 
-    const regex = /^\/(ddb)(?:\/([\w\d]+))?(?:\/([\w\d]+))?(?:\/([\w\d\/]+))?$/;
+    //const regex = /^\/(ddb)(?:\/([\w\d]+))?(?:\/([\w\d]+))?(?:\/([\w\d\/]+))?$/;
+    const regex = /\/(ddb\/?)([\w\d]+\/?)?([\w\d]+\/?)?([\w\d\/]+)?/;
     const url = new URL(event.request.url);
 
     if ((match = url.pathname.match(regex)) !== null) {
+        console.log(match)
         switch (true) {
             case !!match[4]: // keypath
                 break;
@@ -188,10 +210,10 @@ self.addEventListener("fetch", async (event) => {
                 event.respondWith(handle_store_requests(match[2], match[3], event));
                 break;
             case !!match[2]: // dbname
-                event.respondWith(handle_database_requests(match[2], event));
+                event.respondWith(handle_database_requests(event.request, match[2]));
                 break;
             case !!match[1]: // ddb
-                event.respondWith(handle_ddb_requests(event));
+                event.respondWith(handle_ddb_requests(event.request, match[1]));
                 break;
             default:
                 console.error('realy?')
